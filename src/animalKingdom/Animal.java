835c080f22dd76f199cfacfb59dc9d42c3193a46
allisonkydy <allisnelly@gmail.com>
2019-10-02 15:33:07 -0500

    add getters and setters to Animal class

--- src/animalKingdom/Animal.java
+++ src/animalKingdom/Animal.java
@@ -17,10 +17,36 @@ public abstract class Animal
     this.yearDiscovered = yearDiscovered;
   }
   
-  // method with default behavior
+  // methods with default behavior
   public String consumeFood()
   {
     return (name + " eats some food");
+  }
+
+  // getters and setters
+  public int getNumber()
+  {
+    return number;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public int getYearDiscovered()
+  {
+    return yearDiscovered;
+  }
+
+  public void setYearDiscovered(int yearDiscovered)
+  {
+    this.yearDiscovered = yearDiscovered;
   }
 
   // abstract methods - implemented in children
