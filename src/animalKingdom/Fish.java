c3e0fe61ccf7912e9dc97feaa9c654bd7715b75c
allisonkydy <allisnelly@gmail.com>
2019-10-02 14:43:14 -0500

    add constructor and methods to Fish class

--- src/animalKingdom/Fish.java
+++ src/animalKingdom/Fish.java
@@ -1,6 +1,29 @@
 package animalKingdom;
 
-public class Fish
+public class Fish extends Animal
 {
-  
+  // constructor
+  public Fish(String name, int yearDiscovered)
+  {
+    super(name, yearDiscovered);
+  }
+
+  // methods
+  @Override
+  public String move()
+  {
+    return (name + " swims");
+  }
+
+  @Override
+  public String breathe()
+  {
+    return (name + " breathes with gills");
+  }
+
+  @Override
+  public String reproduce()
+  {
+    return (name + " lays eggs");
+  }
 }
