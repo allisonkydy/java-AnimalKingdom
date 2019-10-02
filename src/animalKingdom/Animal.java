41d9d034f36e4f7435725d4141c09aa59d2afbf2
allisonkydy <allisnelly@gmail.com>
2019-10-02 14:25:26 -0500

    add fields, constructor, methods for Animal

--- src/animalKingdom/Animal.java
+++ src/animalKingdom/Animal.java
@@ -2,5 +2,29 @@ package animalKingdom;
 
 public abstract class Animal
 {
+  // fields
+  protected static int maxNumber = 0;
+  protected int number;
+  protected String name;
+  protected int yearDiscovered;
 
+  // constructor
+  public Animal(String name, int yearDiscovered)
+  {
+    maxNumber++;
+    number = maxNumber;
+    this.name = name;
+    this.yearDiscovered = yearDiscovered;
+  }
+  
+  // method with default behavior
+  public String consumeFood()
+  {
+    return (name + " eats some food");
+  }
+
+  // abstract methods - implemented in children
+  public abstract String move();
+  public abstract String breathe();
+  public abstract String reproduce();
 }
