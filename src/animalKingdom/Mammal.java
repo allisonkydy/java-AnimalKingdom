4db69871d28b66890f52eb08957730ebd66f1b05
allisonkydy <allisnelly@gmail.com>
2019-10-02 14:44:24 -0500

    add constructor and methods to Mammal class

--- src/animalKingdom/Mammal.java
+++ src/animalKingdom/Mammal.java
@@ -1,6 +1,29 @@
 package animalKingdom;
 
-public class Mammal
+public class Mammal extends Animal
 {
-  
+  // constructor
+  public Mammal(String name, int yearDiscovered)
+  {
+    super(name, yearDiscovered);
+  }
+
+  // methods
+  @Override
+  public String move()
+  {
+    return (name + " walks");
+  }
+
+  @Override
+  public String breathe()
+  {
+    return (name + " breathes with lungs");
+  }
+
+  @Override
+  public String reproduce()
+  {
+    return (name + " gives birth to live young");
+  }
 }
