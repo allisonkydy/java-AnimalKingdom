1817d4307433142949372eecac190b672ad24780
allisonkydy <allisnelly@gmail.com>
2019-10-02 14:42:13 -0500

    add constructor and methods to Bird class

--- src/animalKingdom/Bird.java
+++ src/animalKingdom/Bird.java
@@ -1,6 +1,29 @@
 package animalKingdom;
 
-public class Bird
+public class Bird extends Animal
 {
+  // constructor
+  public Bird(String name, int yearDiscovered)
+  {
+    super(name, yearDiscovered);
+  }
 
+  // methods
+  @Override
+  public String move()
+  {
+    return (name + " flies");
+  }
+
+  @Override
+  public String breathe()
+  {
+    return (name + " breathes with lungs");
+  }
+
+  @Override
+  public String reproduce()
+  {
+    return (name + " lays eggs");
+  }
 }
